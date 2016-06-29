77556eccc6d783d3f8b157c00560435e4edfffdf
devbox <vfxlord@gmail.com>
2016-06-29 16:43:03 +0300

    implementing checks mannager

---
+++ source/checksManager.js
@@ -0,0 +1,90 @@
+import configs from "../configs.json"
+import fs from "fs"
+
+export default class ChecksManager {
+    constructor() {
+        this._checks = {};
+        this._ttlTimeouts = {};
+        this._checksJsonLocation = `${__dirname}/../${configs.checksFileLocation}`;
+        let isChecksJsonAvalibe = fs.existsSync(this._checksJsonLocation);
+        if (isChecksJsonAvalibe && process.env.NODE_ENV != "test") {
+            this.loadChecks(() => {
+                this._createTTLTimeoutsForAllChecks();
+            });
+
+        }
+        console.log("checksManager created");
+    }
+    delete(check_name) {
+        delete this._checks[check_name];
+        this._removeTTLTimeout(check_name);
+    }
+    upsert(check_name, ttl) {
+        this._checks[check_name] = {
+            live: true,
+            ttl: ttl
+        }
+        this._renewTTLTimeout(check_name, ttl);
+    }
+    getAllChecks() {
+        let returnChecks = [];
+        for (let checkId in this._checks) {
+            if (this._checks.hasOwnProperty(checkId)) {
+                let check = {
+                    name: checkId,
+                    live: this._checks[checkId].live
+                }
+                returnChecks.push(check);
+            }
+        }
+        return returnChecks;
+    }
+    getFalling() {
+        let fallingChecks = [];
+        for (let checkId in this._checks) {
+            if (this._checks.hasOwnProperty(checkId)) {
+                let check = this._checks[checkId];
+                if (!check.live)
+                    fallingChecks.push(checkId);
+            }
+        }
+        return fallingChecks;
+    }
+    loadChecks(callback) {
+        fs.readFile(this._checksJsonLocation, (err, data) => {
+            if (err) throw err;
+            this._checks = JSON.parse(data);
+            console.log("loaded Data: ", JSON.parse(data));
+            callback();
+        });
+    }
+    saveChecks() {
+        fs.writeFile(this._checksJsonLocation, this._checks, (err) => {
+            if (err) throw err;
+            console.log('checks saved!');
+        });
+    }
+    _failCheck(check_name) {
+        let check = this._checks[check_name];
+        if (check)
+            check.live = false;
+    }
+    _addTTLTimeout(check_name, ttl) {
+        this._ttlTimeouts[check_name] = setTimeout(() => this._failCheck(check_name), ttl);
+    }
+    _removeTTLTimeout(check_name) {
+        clearTimeout(this._ttlTimeouts[check_name]);
+    }
+    _renewTTLTimeout(check_name, ttl) {
+        this._removeTTLTimeout(check_name);
+        this._addTTLTimeout(check_name, ttl);
+    }
+    _createTTLTimeoutsForAllChecks() {
+        for (let check_name in this._checks) {
+            if (this._checks.hasOwnProperty(check_name)) {
+                let check = this._checks[check_name];
+                this._addTTLTimeout(check_name, check.ttl);
+            }
+        }
+    }
+}
--- source/routes.js
+++ source/routes.js
@@ -1,28 +1,58 @@
+import timeInMS from "ms";
+import ChecksManager from "./checksManager";
+import HTTPStatus from "http-status";
+
 export default class Routes {
     constructor() {
+        this._checksManager = new ChecksManager();
         console.log("routes created");
     }
     get helloWorldRoute() { return this._helloWorldRoute; }
-    get getChecks() { return this._getChecks; }
-    get postCheck() { return this._postCheck; }
-    get deleteCheck() { return this._deleteCheck; }
-    get getFailing() { return this._getFailing; }
+    get getChecks() { return (req, res) => this._getChecks(req, res); }
+    get postCheck() { return (req, res) => this._postCheck(req, res); }
+    get deleteCheck() { return (req, res) => this._deleteCheck(req, res); }
+    get getFailing() { return (req, res) => this._getFailing(req, res); }
 
     _helloWorldRoute(req, res) {
         res.send('Hello World!');
     }
     _postCheck(req, res) {
-        let wantedCheckId = req.params.check_name;
-        res.send(`${wantedCheckId}-added`);
+        let wantedCheckName = req.params.check_name;
+        let checkTTL = req.query.ttl;
+        if (this._isCheckNameValid(wantedCheckName) && this._isTTLValid(checkTTL)) {
+            checkTTL = timeInMS(`${checkTTL}s`);
+            res.send(`${wantedCheckName}-ttl(${checkTTL})-added`);
+        } else {
+            res.status(HTTPStatus.NOT_ACCEPTABLE).send(`incurrect input at ${wantedCheckName},${checkTTL}`);
+        }
     }
     _deleteCheck(req, res) {
-        let wantedCheckId = req.params.check_name;
-        res.send(`${wantedCheckId}-removed`);
+        let wantedCheckName = req.params.check_name;
+        this._checksManager.delete(wantedCheckName)
+        res.send(`${wantedCheckName}-removed`);
     }
     _getChecks(req, res) {
-        res.send({});
+        res.send(this._checksManager.getAllChecks());
     }
     _getFailing(req, res) {
-        res.send({});
+        res.send(this._checksManager.getFalling());
+    }
+    _isTTLValid(ttl) {
+        try {
+            if (ttl === undefined || ttl === null || parseInt(ttl) <= 0)
+                return false;
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+    _isCheckNameValid(check_name) {
+        try {
+            if (check_name === undefined || check_name === null)
+                return false;
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 }
